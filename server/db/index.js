ef131fba3e170555b073188989191042be3aa9b6
Jinyan <jinyan.shen21@gmail.com>
2018-12-13 17:35:15 -0500

    models created, database seeded

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,23 @@
 const db = require('./db')
-// require each of your models here...
+// require each of the models
+const Album = require('./album')
+const Artist = require('./artist')
+const Song = require('./song')
 
-// ...and give them some nice associations here!
+//specify associations of the models
+Song.belongsTo(Album)
+Album.hasMany(Song)
+
+Song.belongsTo(Artist)
+Artist.hasMany(Song)
+
+Album.belongsTo(Artist)
+Artist.hasMany(Album)
+
 
 module.exports = {
   db,
-  // Include your models in your module.exports as well!
-  // The seed file expects to find them there!
+  Album,
+  Artist,
+  Song
 }
