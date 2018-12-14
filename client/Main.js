3dcccf10fefde760b99c17c165d01f04bd40c044
Jinyan <jinyan.shen21@gmail.com>
2018-12-14 13:40:55 -0500

    allAlbums wireframes created

--- client/Main.js
+++ client/Main.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Sidebar from './Sidebar'
+import AllAlbums from './AllAlbums'
+import Player from './Player'
 
 export default class Main extends React.Component {
   render () {
     return (
       <div id='main' className='row container'>
-        {/* The music starts here! */}
+        <Sidebar />
+        <AllAlbums />
+        <Player />
       </div>
     )
   }
