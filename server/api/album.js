6aee89243b73382fb0752345efab7e974e4bfd0a
Jinyan <jinyan.shen21@gmail.com>
2018-12-13 20:57:28 -0500

    api route /albums/:albumId created

--- server/api/album.js
+++ server/api/album.js
@@ -1,13 +1,26 @@
 const router = require('express').Router()
 const {Album, Artist, Song} = require('../db')
 
-//Get all albums
+//Get api/albums
 router.get('/', async(req, res, next) => {
     try {
         const albums = await Album.findAll({
             include: [Artist]
         })
         res.json(albums)
+    } catch (err){
+        next(err)
+    }
+})
+
+//Get api/albums/:albumId
+router.get('/:albumId', async(req, res, next) => {
+    try {
+        const album = await Album.findById(+req.params.albumId,
+            {include: [Artist,
+                {model: Song, include: [Artist]}
+            ]})
+        res.json(album)
     } catch (err){
         next(err)
     }
